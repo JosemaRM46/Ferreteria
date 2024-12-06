78ab046abf96178aa3e2ecba781da3723a0dd2ff
JosemaRM46 <romeromanuel@unah.hn>
2024-12-06 17:22:06 -0600

    Variables de entorno backend

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
@@ -8,11 +9,11 @@ app.use(cors());
 app.use(express.json()); // Middleware para manejar JSON
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'ferreteria',
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 connection.connect(err => {
